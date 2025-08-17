e903054d9a34bdbbf9c446bd314cb980453406a6
Devin Cooper <devinraycooper@gmail.com>
2025-08-16 20:02:36 -0400

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'not-todo-app',
+  appId: 'com.darcsoftware.nottodo',
+  appName: 'Not Todo',
   webDir: 'www',
   plugins: {
     LocalNotifications: {
